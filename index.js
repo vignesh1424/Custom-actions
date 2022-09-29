0c51a2664fe11c0d91a64a39409d3defad6099ed
Vignesh.s <vignesh.s@medtechsoft.co>
2022-09-29 17:19:42 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   setFailed(error.message);
 }
