fb4315febdb54232c2c62818640684e703846f46
Vignesh.s <vignesh.s@medtechsoft.co>
2022-09-29 18:28:11 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { getInput, setOutput, setFailed } from '@actions/core';
+const core = require('@actions/core');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = getInput('who-to-greet');
+  const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  setOutput("time", time);
+  core.setOutput("time", time);
 } catch (error) {
-  setFailed(error.message);
+  core.setFailed(error.message);
 } 
