632b3e1b6358ff25dc89951ca420cb4165b1b2d4
Vignesh.s <vignesh.s@medtechsoft.co>
2022-11-02 12:31:12 +0530

    aded custom actions to fetch the credentials

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { getInput, setOutput, setFailed } from '@actions/core';
 try {
   // `who-to-greet` input defined in action metadata file
   const awsregion = getInput('aws-region');
-  console.log(`aws-region ${nameToGreet}!`);
+  console.log(`aws-region ${awsregion}!`);
   const accesskey = getInput('aws-region');
   console.log(`aws-access-key ${accesskey}!`);
   const secretaccesskey = getInput('aws-region');
